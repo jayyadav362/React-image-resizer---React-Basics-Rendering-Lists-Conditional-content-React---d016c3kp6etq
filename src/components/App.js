9a2fa588b09e61915987b1544a8b5488c529ccbf
Jay Kumar Bharti <60088194+jayyadav362@users.noreply.github.com>
2023-01-27 16:57:30 +0530

    latest

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
+  const [height , setHeight]=useState(320);
+  const [width , setWidth]=useState(320);
+
   return (
     <div id="main">
-
+        <input type="range" min="100" max="800" value={`${height}px`} id="height-slider" onChange={(e)=>setHeight(e.target.value)}></input>
+        <div>{height}px</div>
+        <input type="range" min="100" max="800" value={`${width}px`} id="width-slider" onChange={()=>setWidth(e.target.value)}></input>
+        <div>{width}px</div>
+        <img id='resizable-img' src='../src/image.jpeg' height={height} width={width} alt="resizable-img"/>
     </div>
   )
 }
