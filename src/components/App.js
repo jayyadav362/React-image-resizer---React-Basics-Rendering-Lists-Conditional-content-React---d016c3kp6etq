b8ddca163cdc70a7ab83065403ff6cec699f04c6
Jay Kumar Bharti <60088194+jayyadav362@users.noreply.github.com>
2023-01-27 17:01:05 +0530

    latest

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
     <div id="main">
         <input type="range" min="100" max="800" value={height} id="height-slider" onChange={(e)=>setHeight(e.target.value)}></input>
         <div>{height}px</div>
-        <input type="range" min="100" max="800" value={width} id="width-slider" onChange={()=>setWidth(e.target.value)}></input>
+        <input type="range" min="100" max="800" value={width} id="width-slider" onChange={(e)=>setWidth(e.target.value)}></input>
         <div>{width}px</div>
         <img id='resizable-img' src='../src/image.jpeg' height={`${height}px`} width={`${width}px`} alt="resizable-img"/>
     </div>
